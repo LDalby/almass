86609494302cac1ee470a72d91749cd233cd628a
LDalby <lars@bios.au.dk>
2015-06-25 13:54:03 +0200

    Fix write when multiple variables

--- PreRunSetup.r
+++ PreRunSetup.r
@@ -3,20 +3,23 @@
 # Author: Lars Dalby
 # Date: 23 June 2015
 
-setwd('c:/MSVC/WorkDirectory/')
+setwd('d:/almass/WorkDirectories/HunterModelTesting/')  # The run directory
 
 # To get the line number in the parameter list in multi parameter scenarios we make a vector of line numbers for the
 # first of the parameters in each run (this approach is also used for single parameter scenarios):
-runs = 100  # The number of runs
-params = 2  # The number of paramters being modified per run 
-lineno = seq(1, runs*params, params)
+runs = 228  # The number of runs
+numberofparams = 2  # The number of paramters being modified per run 
+lineno = seq(1, runs*numberofparams, numberofparams)
 
 params = readLines('ParameterValues.txt')
 
 counter = as.numeric(readLines('counter.txt'))
 
 write(params[lineno[counter]], 'TIALMaSSConfig.cfg', append = TRUE)
-if(length(params) == 2)
+if(numberofparams > 1)
 {
-	write(params[lineno[counter]+1], 'TIALMaSSConfig.cfg', append = TRUE)
+	for (i in 2:numberofparams) 
+	{
+		write(params[lineno[counter]+1], 'TIALMaSSConfig.cfg', append = TRUE)
+	}
 }
