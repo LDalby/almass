e2c9df8b4327f5f755c25b122fad3907ec807482
LDalby <lars@bios.au.dk>
2015-08-07 08:37:24 +0200

    Add script to distribute files across a directories

---
+++ FileDist.r
@@ -0,0 +1,14 @@
+# Cooy file to x number of directories
+# Just handy when e.g. a new exe needs to distributed
+library(R.utils)
+
+# List the directories:
+pathtodirs = 'd:/almass/WorkDirectories/Hunter/HunterTestingAug2015/'
+dirs = dir(pathtodirs)  # For this to work you can't have a bunch of crap sitting in
+						# in pathtodirs. Only the subdirectories
+
+filetodist = 'c:/MSVC/ALMaSS_CmdLine/x64/Release/ALMaSS_CmdLine.exe'  # The file that needs copying
+
+for (i in seq_along(dirs)) {
+	file.copy(filetodist, to = paste0(pathtodirs, dirs[i]), overwrite = TRUE)
+}
