574a59eea8be852ae188747bc38154dd0c74aa2b
LDalby <lars@bios.au.dk>
2015-09-17 22:36:36 +0200

    Fix dublicated naming

--- PreRunSetup.r
+++ PreRunSetup.r
@@ -3,14 +3,16 @@
 # Author: Lars Dalby
 # Date: 23 June 2015
 
+library(data.table)
+
 # Setup work directory (done automatically when distributing the files, therefore blank):
 
 # To get the line number in the parameter list in multi parameter scenarios we make a vector of line numbers for the
 # first of the parameters in each run (this approach is also used for single parameter scenarios):
 paramvals = fread('ParameterValues.txt')  # To figure out how many runs we have
-params = nrow(unique(paramvals[, 1, with = FALSE])) # The number of paramters being modified per run 
-runs = nrow(paramvals)/params
-lineno = seq(1, runs*params, params)
+numberofparams = nrow(unique(paramvals[, 1, with = FALSE])) # The number of paramters being modified per run 
+runs = nrow(paramvals)/numberofparams
+lineno = seq(1, runs*numberofparams, numberofparams)
 
 # This is needed again as we this time read it in as single lines (and not as a data.table)
 params = readLines('ParameterValues.txt')
