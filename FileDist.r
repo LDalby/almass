c49e0abb9481b7bcd5f163fde199364c05516e18
LDalby <lars@bios.au.dk>
2015-09-17 22:28:48 +0200

    Restructure loops

--- FileDist.r
+++ FileDist.r
@@ -15,19 +15,19 @@ file1 = 'c:/MSVC/ALMaSS_CmdLine/x64/Release/ALMaSS_CmdLine.exe'
 file2 = 'C:/Users/lada/Git/almass/counter.txt' 
 file3 = 'C:/Users/lada/Git/almass/batchr.r'	
 file4 = 'C:/Users/lada/Git/almass/ErrorFile.txt'
-# file5 = 'C:/Users/lada/Git/almass/PreRunSetup.r'
+file5 = 'C:/Users/lada/Git/almass/PreRunSetup.r'
 
-# filestodist = c(file1, file2, file3, file4, file5)
-filestodist = c(file1, file2, file3, file4)
+filestodist = c(file1, file2, file3, file4, file5)
+# filestodist = c(file1, file2, file3, file4)
 
 # We overwrite, so be sure you actually want to do this!
 for (i in seq_along(dirs)) {
+	wd = paste0(pathtodirs, dirs[i])
 	for (j in seq_along(filestodist)) {
-		wd = paste0(pathtodirs, dirs[i])
 		file.copy(filestodist[j], to = wd, overwrite = TRUE)
-		AppendWorkDir(WorkDir = wd, InScript = file3, OutScript = 'batchr.r') 
-		AppendWorkDir(WorkDir = wd, InScript = file5, OutScript = 'PreRunSetup.r') 
 	}
+	AppendWorkDir(WorkDir = wd, InScript = file3, OutScript = 'batchr.r') 
+	AppendWorkDir(WorkDir = wd, InScript = file5, OutScript = 'PreRunSetup.r') 
 }
 
 #------ Below here we ditribute the different parameters ------#
