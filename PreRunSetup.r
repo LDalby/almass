4f55cde822fe6f0a64ed2929c00ef1cb23a05767
LDalby <lars@bios.au.dk>
2015-09-17 21:07:15 +0200

    Also automate wd, params and run settings here

--- PreRunSetup.r
+++ PreRunSetup.r
@@ -3,16 +3,18 @@
 # Author: Lars Dalby
 # Date: 23 June 2015
 
-setwd('d:/almass/WorkDirectories/Hunter/HunterTestingAug2015/WD0/')  # The run directory
+# Setup work directory (done automatically when distributing the files, therefore blank):
 
 # To get the line number in the parameter list in multi parameter scenarios we make a vector of line numbers for the
 # first of the parameters in each run (this approach is also used for single parameter scenarios):
-runs = 5  # The number of runs
-numberofparams = 1  # The number of paramters being modified per run 
-lineno = seq(1, runs*numberofparams, numberofparams)
+paramvals = fread('ParameterValues.txt')  # To figure out how many runs we have
+params = nrow(unique(paramvals[, 1, with = FALSE])) # The number of paramters being modified per run 
+runs = nrow(paramvals)/params
+lineno = seq(1, runs*params, params)
 
+# This is needed again as we this time read it in as single lines (and not as a data.table)
 params = readLines('ParameterValues.txt')
-
+                  
 counter = as.numeric(readLines('counter.txt'))
 if(counter == 1)
 {
