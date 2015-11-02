e2fd82f760d74d93c92ccdc7787739eaffe8dc6b
LDalby <lars@bios.au.dk>
2015-11-02 12:16:44 +0100

    Remove obsolete param type specification
    Obsolete after update to the ralmass package

--- FileDist.r
+++ FileDist.r
@@ -37,50 +37,50 @@ for (i in seq_along(dirs)) {
 # 0
 val = seq(100, 500, length.out = 5)
 setwd(paste0(pathtodirs, dirs[1]))  
-GenerateParams('GOOSE_MINFORAGEOPENNESS (float)' = val, write = TRUE)
+GenerateParams('GOOSE_MINFORAGEOPENNESS' = val, write = TRUE)
 # 1
 setwd(paste0(pathtodirs, dirs[2]))  # The first two in fact ignore these
-GenerateParams('GOOSE_MINFORAGEOPENNESS (float)' = val, write = TRUE)
+GenerateParams('GOOSE_MINFORAGEOPENNESS' = val, write = TRUE)
 # 2 
 val = seq(100, 1000, length.out = 10)
 setwd(paste0(pathtodirs, dirs[3])) 
-GenerateParams('GOOSE_MINFORAGEOPENNESS (float)' = val, write = TRUE)
+GenerateParams('GOOSE_MINFORAGEOPENNESS' = val, write = TRUE)
 # 3
 val = seq(0, 2.2, length.out = 10)
 setwd(paste0(pathtodirs, dirs[4])) 
-GenerateParams('HUNTERS_MAXDENSITY (float)' = val, write = TRUE)
+GenerateParams('HUNTERS_MAXDENSITY' = val, write = TRUE)
 # 4
 val = seq(100, 1000, length.out = 10)
 setwd(paste0(pathtodirs, dirs[5])) 
-GenerateParams('GOOSE_MINFORAGEOPENNESS (float)' = val, write = TRUE)
+GenerateParams('GOOSE_MINFORAGEOPENNESS' = val, write = TRUE)
 # 5
 val = seq(0, 2.2, length.out = 10)
 setwd(paste0(pathtodirs, dirs[6])) 
-GenerateParams('HUNTERS_MAXDENSITY (float)' = val, write = TRUE)
+GenerateParams('HUNTERS_MAXDENSITY' = val, write = TRUE)
 # 6
 val = seq(700, 1900, length.out = 13)
 val2 = seq(1.18, 2.5, length.out = 20)
 setwd(paste0(pathtodirs, dirs[7])) 
-GenerateParams('GOOSE_MINFORAGEOPENNESS (float)' = val,
-				 'HUNTERS_MAXDENSITY (float)' = val2, write = TRUE)
+GenerateParams('GOOSE_MINFORAGEOPENNESS' = val,
+				 'HUNTERS_MAXDENSITY' = val2, write = TRUE)
 EditBat(paste0(pathtodirs, dirs[7]))
 # 7 same values as 6
 setwd(paste0(pathtodirs, dirs[8])) 
-GenerateParams('GOOSE_MINFORAGEOPENNESS (float)' = val,
-				 'HUNTERS_MAXDENSITY (float)' = val2, write = TRUE)
+GenerateParams('GOOSE_MINFORAGEOPENNESS' = val,
+				 'HUNTERS_MAXDENSITY' = val2, write = TRUE)
 EditBat(paste0(pathtodirs, dirs[8]))
 # 8
 setwd(paste0(pathtodirs, dirs[9])) 
 val = seq(-0.001, -0.01, length.out = 100)
-GenerateParams('CLOSESTFARMPROBPARAMONE (float)' = val, write = TRUE)
+GenerateParams('CLOSESTFARMPROBPARAMONE' = val, write = TRUE)
 EditBat(paste0(pathtodirs, dirs[9]))
 
 # 9
 setwd(paste0(pathtodirs, dirs[10])) 
 val = seq(-0.01, -0.2, length.out = ?)
 val2 = seq(100, 1000, length.out = 10)
-GenerateParams('CLOSESTFARMPROBPARAMONE (float)' = val,
- 'GOOSE_MINFORAGEOPENNESS (float)' = val2,  write = TRUE)
+GenerateParams('CLOSESTFARMPROBPARAMONE' = val,
+ 'GOOSE_MINFORAGEOPENNESS' = val2,  write = TRUE)
 
 # 10
 
