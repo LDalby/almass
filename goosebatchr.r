94092ac7dc858c1c6e2e77a675d43cbb813a6232
LDalby <lars@bios.au.dk>
2016-03-08 12:22:25 +0100

    Finish goose testing and update runparsimsetup file

--- goosebatchr.r
+++ goosebatchr.r
@@ -12,10 +12,11 @@
 #
 # Before running this script check that you have the following files in the run directory:
 # 1: counter.txt
-# 2:
-# 3:
-# 4: batch.r
-# 5: PreRunSetup.r
+# 2: fugledata_20150320_obs_clean.csv
+# 3: observations_PG_01Jan2010-18Jan2016_API.xlsx
+# 4: HabitatUseAll2014.csv
+# 5: goosebatch.r
+# 6: PreRunSetup.r
 # Again, if you used FileDist.r then this should be taken care of.
 # --------------------------------------------------------------------------------------------# 
 
@@ -23,8 +24,10 @@
 library(data.table)
 library(ralmass)
 library(stringr)
-library(reshape2)
-#library(slackr)  # Only needed if you want Slack to give you updates on progress
+library(lubridate)
+library(readxl)
+
+# library(slackr)  # Only needed if you want Slack to give you updates on progress
 
 # Setup work directory (done automatically when distributing the files, therefore blank):
 
@@ -47,155 +50,137 @@ if(counter == 1)
 	# Set up the results directory
 	dir.create('Results')
 	# Set up the headers in first run
-	line = paste('Parameter', 'Value', 'WeightFit', 'FlockSizeFit', 'HabitatUseFit', 
-		 'OverallFit', sep = '\t')
+	line = paste('Parameter', 'Value', 'WeightFit', 'FlockSizeFit', 'FlockSizeFitTimed',
+		'HabitatUseFit',  'OverallFit', sep = '\t')
 	write(line, file = paste0(resultpath, 'ParameterFittingResults.txt'))
 	# Copy the Goose params to the result folder for reference and checking
 	file.copy('ParameterValues.txt', resultpath, copy.date = TRUE)
 }
 
-# If there is no results from the sim because no hunters were distributed:
-if(length(grep("Hunter_Hunting_Locations.txt", dir())) == 0)
+# If there the simulation was terminated before writing results (should not happen):
+if(length(grep("GooseFieldForageData.txt", dir())) == 0)
 {
 	lines = readLines('ParameterValues.txt')
 	for (i in 1:numberofparams) {
 		param = word(lines[lineno[counter]+(i-1)], 1)  # Get the parameter name
 		value = as.numeric(str_split(lines[lineno[counter]+(i-1)], '=')[[1]][2])  # Get the value
-		line = paste(param, value, NA, NA, NA, NA, sep = '\t')
+		line = paste(param, value, NA, NA, NA, NA, NA, sep = '\t')
 		write(line, file = paste0(resultpath, 'ParameterFittingResults.txt'), append = TRUE)
 	}
 }
-
-#¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤ Distances ¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤#
-
-if(length(grep("Hunter_Hunting_Locations.txt", dir())) != 0)
+# --------------------------------------------------------------------------------------------#
+#                                    Flock sizes                                              #
+# --------------------------------------------------------------------------------------------#
+resultpath = 'c:/MSV/WorkDirectory/'
+forage = fread(paste(resultpath, 'GooseFieldForageData.txt', sep = ''))
+forage = fread('GooseFieldForageData.txt', showProgress = FALSE)
+if(length(grep("GooseFieldForageData.txt", dir())) != 0)
 {
-	# Simulation results:
-	locations = fread('Hunter_Hunting_Locations.txt')
-	filename = paste0(resultpath,'HuntingLocationsRun', counter, '.txt')
-	write.table(locations, file = filename, row.names = FALSE, sep = '\t')
+	# Field data:
+	fieldobs = fread('fugledata_20150320_obs_clean.csv')
+	fieldobs = fread('o:/ST_GooseProject/Field data/Fugledata/fugledata_20150320_obs_clean.csv')
+	flocks = fieldobs[Month %in% c(12,1) & Hour >= 11 & Hour <= 13,]  # todo: Why do we only use december and jan?
+	flocks[, c('Month', 'Hour'):=NULL]
+	setcolorder(flocks, c('Species', 'Numbers', 'Type'))
 
-	farms = fread('Hunter_Hunting_Locations_Farms.txt')
-	filename = paste0(resultpath, 'HuntingLocationsFarmRun', counter, '.txt')
-	write.table(farms, file = filename, row.names = FALSE, sep = '\t')
+	# Simulation results - max number on a day:
+	simflocks = forage[Geese > 0, .(Day, Barnacle, Pinkfoot, Greylag)]
+	melted = data.table::melt(simflocks, id.vars = 'Day', variable.name = 'Species', value.name = 'Numbers')
+	melted = melted[Numbers != 0,]
+	melted[, Day:=NULL]
+	melted[, Type:='Simulated']
+	dists = rbind(melted, flocks)
+
+	DegreeOverlapB = round(CalcOverlab(dists, species = 'Barnacle'), digits = 2)
+	DegreeOverlapP = round(CalcOverlab(dists, species = 'Pinkfoot'), digits = 2)
+	DegreeOverlapG = round(CalcOverlab(dists, species = 'Greylag'), digits = 2)
+
+	# Simulation results - timed counts:
+	simflocks = forage[Geese > 0, .(Day, BarnacleTimed, PinkfootTimed, GreylagTimed)]
+	melted = data.table::melt(simflocks, id.vars = 'Day', variable.name = 'Species', value.name = 'Numbers')
+	melted = melted[Numbers != 0,]
+	melted[ ,Day:=NULL]
+	melted[, Type:='Simulated']
+	distsTimed = rbind(melted, flocks)
 	
-	idvars = c('HunterID','HunterType','HomeX','HomeY','NoFarmrefs')
-	dist = rep(NA, nrow(locations))
-	for (j in seq_along(locations[,HunterID])) 
-	{
-		huntinglocs = melt(locations, id.vars = idvars)[HunterID == j-1 & !is.na(value)][,value]
-		temp = rep(NA, length(huntinglocs))
-		for (i in seq_along(huntinglocs))
-		{
-			thedist = dist(rbind(as.numeric(locations[HunterID == j-1,.(HomeX, HomeY)]),
-				as.numeric(farms[FarmRef == huntinglocs[i],.(FarmCentroidX, FarmCentroidY)])))
-			temp[i] = thedist
-		}
-		dist[j] = min(temp)
-	}
-	locations[, Dists:=dist/1000]
+	DegreeOverlapBT = round(CalcOverlab(distsTimed, species = 'Barnacle'), digits = 2)
+	DegreeOverlapPT = round(CalcOverlab(distsTimed, species = 'Pinkfoot'), digits = 2)
+	DegreeOverlapGT = round(CalcOverlab(distsTimed, species = 'Greylag'), digits = 2)
+# --------------------------------------------------------------------------------------------#
+#                                    Weights                                                  #
+# --------------------------------------------------------------------------------------------#
+	mass = fread('GooseEnergeticsData.txt', showProgress = FALSE)
+	mass[,Day:=Day-365]
+	api = read_excel('observations_PG_01Jan2010-18Jan2016_API.xlsx')
+	api = as.data.table(api)
+	api = api[SEXE == 'M']
+	field = CleanAPIData(api)
+	Weightfit = CalcWeightFit(mass, field)
 
-	locations[Dists < 1, Bin:=0]
-	locations[Dists <= 10 & Dists >= 1, Bin:=10]
-	locations[Dists <= 20 & Dists > 10, Bin:=20]
-	locations[Dists <= 40 & Dists > 20, Bin:=40]
-	locations[Dists <= 60 & Dists > 40, Bin:=60]
-	locations[Dists <= 80 & Dists > 60, Bin:=80]
-	locations[Dists <= 100 & Dists > 80, Bin:=100]
-	locations[Dists <= 150 & Dists > 100, Bin:=150]
-	locations[Dists <= 200 & Dists > 150, Bin:=200]
-	locations[Dists > 200, Bin:=201]
-
-	# The survey results:
-	huntersurvey = fread('HunterSurveyResultsDistanceJuly2015.txt')
-	# The simulation results needs to be binned manually and added to the survey data:
-	TheBins = c(0,10,20,40,60,80,100,150,200,201)
-	for (i in seq_along(TheBins)) {
-		huntersurvey[Bin == TheBins[i], ModelRes:= length(locations[Bin == TheBins[i], Bin])]
-	}
-	huntersurvey[, propSim:=ModelRes/sum(ModelRes)]
-	huntersurvey[, propSur:=RespondTH_JM/sum(RespondTH_JM)]
-
-	distancefit = with(huntersurvey, 1-sum((propSim-propSur)^2))
-
-    #¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤ Density ¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤#
-	# Load the survey Results
-	survey = fread('HunterSurveyResultsDensity.csv')
-
-	farms[,Numbers:=NoHunters/(FarmSize/10000)]
-	farms[,Type:= 'Simulated']
-	simulated = farms[Numbers > 0, c('Numbers', 'Type'), with = FALSE]
-	# Collect the survey and sim results:
-	density = rbind(survey, simulated)
-	# Asses the fit
-	overlab = round(CalcOverlab(density, species = 'Hunter'), 3)  #see ?CalcOverlab for documentation
-	# Find the maximum number of hunters on any farm:
-	maxhunters = max(farms[,NoHunters])
-
-    #------------ Number of hunters per farm ------------#
-	# Load the survey results
-	survey = fread('HunterSurveyResultsFarm.csv')
-
-	simulated = farms[NoHunters > 0, .(NoHunters, Type)]
-	setnames(simulated, old = 'NoHunters', new = 'Numbers')
-	no.hunters = rbind(survey, simulated)
-	no.huntersFitOverlab = round(CalcOverlab(no.hunters, species = 'Hunter'), 3)
+# --------------------------------------------------------------------------------------------#
+#                                    Habitat use                                              #
+# --------------------------------------------------------------------------------------------#
+	FieldData = fread('HabitatUseAll2014.csv')
+	FieldData[HabitatUse == 'Stubble undersown', HabitatUse:='Grass']
+	FieldData[HabitatUse == 'UnharvestedBarley', HabitatUse:='Stubble']
+	FieldData[, NMTotal:=sum(N), by=.(Month, Species)]
+	FieldData[, Prop:=N/NMTotal]
+	FieldData[rep(seq(.N),N), .(Month, HabitatUse, N, Species, NMTotal, Prop), by = Species]
 	
-	# Difference in mean number of hunters per farm
-	avg.no.hunt = mean(survey[,Numbers])
-	avg.simulated = mean(simulated[,Numbers])
-	MeanDiffNoHunt = abs(avg.no.hunt - avg.simulated)
-	# Least squares
-	survey[, N:=.N, by = Numbers]
-	survey = unique(survey)
-	setnames(survey, old = 'Numbers', new = 'Bin')
-	bins = data.table('N' = c(0,0), 'Type' = rep('Fieldobs',2), 'Bin' = c(8, 12))
-	survey = rbind(survey, bins)
-	setkey(survey, Bin)
-	# The simulation results needs to be binned manually and added to the survey data:
-	for (i in 1:11) {
-	survey[Bin == i, ModelRes:= length(farms[NoHunters == i, NoHunters])]
-	}
-	survey[Bin == 12, ModelRes:= length(farms[NoHunters >= 12, NoHunters])]
-
-	survey[, propSim:=ModelRes/sum(ModelRes)]
-	survey[, propSur:=N/sum(N)]
-
-	no.huntersFit = with(survey, 1-sum((propSim-propSur)^2))
+	forage[, Month:=month(as.Date(Day, origin = '2012-01-01'))]
+	# Pinkfoot:
+	hbpf = forage[Month > 8 | Month == 1, .(Pinkfoot, HabitatUsePF, Month)]
+	hbpf[, Species:='Pinkfoot']
+	hbpf = hbpf[complete.cases(hbpf),]
+	setnames(hbpf, old = c('HabitatUsePF', 'Pinkfoot'), new = c('HabitatUse', 'N'))
+	# Greylag:
+	hbgl = forage[Month > 8 | Month == 1, .(Greylag, HabitatUseGL, Month)]
+	hbgl[, Species:='Greylag']
+	hbgl = hbgl[complete.cases(hbgl),]
+	setnames(hbgl, old = c('HabitatUseGL', 'Greylag'), new = c('HabitatUse', 'N'))
+	# Barnacle:
+	hbbn = forage[Month > 8 | Month == 1, .(Barnacle, HabitatUseBN, Month)]
+	hbbn[, Species:='Barnacle']
+	hbbn = hbbn[complete.cases(hbbn),]
+	setnames(hbbn, old = c('HabitatUseBN', 'Barnacle'), new = c('HabitatUse', 'N'))
+	# Full data:
+	hb = rbind(hbpf, hbbn, hbgl)
+	setkeyv(hb, c('Species', 'Month'))
+	
+	hb[, NMTotal:=sum(N), by = .(Month, Species)]
+	hb[, Prop:=N/NMTotal]
+	hb[, Month:=as.factor(Month)]
+	setkeyv(hb, c('Species', 'Month'))
+	
+	HabitatUseFit = CalcHabitatUseFit(FieldData = FieldData, SimData = forage)
 
 	# Calculate the overall model fit
-	OverallFit = distancefit + overlab + no.huntersFit
+	OverallFit = Weightfit + HabitatUseFit + FlockSizeFitTimed
 
 	# Write out the results of the parameter fitting and prepare for next run:
 	lines = readLines('ParameterValues.txt')
 	for (i in 1:numberofparams) {
 		param = word(lines[lineno[counter]+(i-1)], 1)  # Get the parameter name
 		value = as.numeric(str_split(lines[lineno[counter]+(i-1)], '=')[[1]][2])  # Get the value
-		if(param == 'HUNTERS_MAXDENSITY') {
-			AllLegal = CheckDensity(farms, value)
-		}
-		if(param != 'HUNTERS_MAXDENSITY') AllLegal = NA
-		line = paste(param, value, distancefit, overlab, no.huntersFit, no.huntersFitOverlab,
-		 MeanDiffNoHunt, AllLegal, maxhunters, OverallFit, sep = '\t')
+		line = paste(param, value, Weightfit, FlockSizeFit, FlockSizeFitTimed,
+		 HabitatUseFit, OverallFit, OverallFit, sep = '\t')
 		write(line, file = paste0(resultpath, 'ParameterFittingResults.txt'), append = TRUE)
 	}
-
-	# As the last thing we delete the Hunter_Hunting_Locations.txt Hunter_Hunting_Locations_Farms.txt
-	# We do this because almass might exit without distributing hunters. If that happens files from a previous
+	# As the last thing we delete the goose output files
+	# We do this in case almass exits due to an error. If that happens files from a previous
 	# run might still be sitting in the run directory and we would simply analyze these as if they were the new 
 	# run and get results identical to the previous run
 
-	if(length(grep("Hunter_Hunting_Locations.txt", dir())) > 0)
+	if(length(grep("GooseEnergeticsData.txt", dir())) > 0)
 	{
-		file.remove("Hunter_Hunting_Locations.txt")
+		file.remove("GooseEnergeticsData.txt")
 	}
 
-	if(length(grep("Hunter_Hunting_Locations_Farms.txt", dir())) > 0)
+	if(length(grep("GooseFieldForageData.txt", dir())) > 0)
 	{
-		file.remove("Hunter_Hunting_Locations_Farms.txt")
+		file.remove("GooseFieldForageData.txt")
 	}
 }
-
 # If you want updates:
 # slackrSetup(channel="@name", api_token = INSERT YOUR TOKEN)
 # slackr(paste(counter))
