2cb56467e326faaab055419e23b43f720abaffb9
LDalby <lars@bios.au.dk>
2016-04-05 11:37:42 +0200

    Adapt to 8255A

--- FileDist.r
+++ FileDist.r
@@ -4,24 +4,26 @@ library(R.utils)
 library(ralmass)
 
 # List the parent directory of all the work directories
-pathtodirs = 'e:/almass/WorkDirectories/Goose/'
-dirs = dir(pathtodirs)  # For this to work you can't have a bunch of crap sitting in
+pathtodirs = 'u:/Hunter/'
+dirs = dir(pathtodirs)[-grep('BaseWD')]  # For this to work you can't have a bunch of crap sitting in
 						# in pathtodirs. Only the subdirectories
 
 # A common use for this would be to copy a fresh exe along with
 # resetting the counter, clearing the error file and copying
 # the batchr and prerunsetup file.
-file1 = 'c:/MSV/ALMaSS_CmdLine/x64/Release/ALMaSS_CmdLine.exe' 
-file2 = 'C:/Users/lada/Git/almass/counter.txt' 
-file3 = 'C:/Users/lada/Git/almass/goosebatchr.r'	
-file4 = 'C:/Users/lada/Git/almass/ErrorFile.txt'
-file5 = 'C:/Users/lada/Git/almass/PreRunSetup.r'
+file1 = 'o:/ST_GooseProject/ALMaSS/Executables/ALMaSS_CmdLine.exe' 
+file2 = 'C:/Users/lada/git/almass/counter.txt' 
+file3 = 'C:/Users/lada/git/almass/hunterbatchr.r'	
+file4 = 'C:/Users/lada/git/almass/ErrorFile.txt'
+file5 = 'C:/Users/lada/git/almass/PreRunSetup.r'
 # Species specific things:
-file6 = 'o:/ST_GooseProject/Field data/Fugledata/fugledata_20150320_obs_clean.csv'
-file7 = 'o:/ST_GooseProject/Field data/Fugledata/HabitatUseAll2014.csv'
-file8 = 'o:/ST_GooseProject/Field data/observations_PG_01Jan2010-18Jan2016_API.xlsx'
+file6 = 'C:/Users/lada/git/ALMaSS_inputs/GooseManagement/Vejlerne/Hunter/HunterHomeLocations.txt'
+file7 = 'o:/ST_GooseProject/ALMaSS/HunterModelTesting/SurveyResults/HunterSurveyResultsDensity.csv'
+file8 = 'o:/ST_GooseProject/ALMaSS/HunterModelTesting/SurveyResults/HunterSurveyResultsDistanceJuly2015.txt'
+file9 = 'o:/ST_GooseProject/ALMaSS/HunterModelTesting/SurveyResults/HunterSurveyResultsFarm.csv'
 
-filestodist = c(file1, file2, file3, file4, file5, file6, file7, file8)
+filestodist = c(file1, file2, file3, file4, file5, file6, file7, file8, file9)
+
 
 # We overwrite, so be sure you actually want to do this!
 for (i in seq_along(dirs)) {
