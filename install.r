0f297e7a0acce38a35d386dad1e49666e1d0fabf
LDalby <lars@bios.au.dk>
2016-04-06 08:08:38 +0200

    Add pck install script

---
+++ install.r
@@ -0,0 +1,7 @@
+pcks = c('raster', 'devtools', 'rgdal', 'rgeos', 'rasterVis', 'R.utils')
+path = "C:\\Program Files\\R\\R-3.2.4\\library"
+install.packages(pcks, lib = path, dependencies = TRUE)
+
+library(devtools)
+install_github('LDalby/ralmass', lib = path)
+
