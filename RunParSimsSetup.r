3555bf14ce061ca02dd5b4e6b0277269b4971c84
LDalby <lars@bios.au.dk>
2016-08-10 09:08:55 +0200

    minor changes

--- RunParSimsSetup.r
+++ RunParSimsSetup.r
@@ -3,14 +3,15 @@ library(R.utils)
 library(ralmass)
 
 # Setup the directories
-npar = 13  # Number to run in parallel
-basedir = 'e:/almass/WorkDirectories/BaseWD'
+npar = 16 
+basedir = 'e:/almass/WorkDirectories/Goose/WD01/'
 basename = 'WD'
 # resultpath = 'e:/almass/Results/GooseManagement/Goose/Results08032016/' 
 setwd('e:/almass/WorkDirectories/Goose/')
 # Copy the files
-for (i in 1:npar) 
+for (i in 14:npar) 
 {
+	# file.copy(basedir, to = paste0(basename,i))  # Copy the almass work directory
 	copyDirectory(basedir, to = paste0(basename,i))  # Copy the almass work directory
 #	dir.create(paste0(resultpath, basename, i))  # Set up the result storage
 }
