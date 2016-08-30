99a24d28e93e7faeeb1e3dadef229db4a3e74d1d
LDalby <lars@bios.au.dk>
2016-08-30 10:03:16 +0200

    Round check props

--- ConstructHHL.R
+++ ConstructHHL.R
@@ -1,11 +1,15 @@
 # Construct new Hunter_Hunting_Locations.txt file
 # Construct the vector of check frequencies
+library(data.table)
+library(ralmass)
+
 everyday = round(746*.16)
 afewtimes = round(746*.32)
 everyweek = round(746*.19)
 notsooften = round(746*.21)
 never = round(746*.12)
-checkfreq = c(rep(1, everyday), rep(3/7, afewtimes), rep(1/7, everyweek), rep(1/14, notsooften), rep(0, never)) 
+checkfreq = c(rep(1, everyday), rep(round(3/7, 3), afewtimes), rep(round(1/7, 3), everyweek),
+ rep(round(1/14, 3), notsooften), rep(0, never)) 
 length(checkfreq)  # One too long, but we sample it later, so no problem.
 
 HHL = 'C:/MSV/ALMaSS_inputs/GooseManagement/Vejlerne/Hunter/746_vejhunter_behaviour_18-08-2016.txt'
