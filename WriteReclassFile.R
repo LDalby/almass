2c1ed3aed786cf39c966f523400978ec6c18896e
LDalby <lars@bios.au.dk>
2016-09-06 09:14:40 +0200

    Write to R instead of py

--- WriteReclassFile.R
+++ WriteReclassFile.R
@@ -5,9 +5,10 @@
 library(data.table)
 library(ralmass)
 
-simbag = fread('e:/almass/WorkDirectories/Goose/WD11/HuntingBagRecord.txt')
+simbag = fread('e:/almass/WorkDirectories/Goose/WD51/HuntingBagRecord.txt')
 simbag[, Species:=sapply(GameType, ConvertGameType)]
 simbag[, NoShot:=.N, by = list(PolygonRef, Species, SeasonNumber)]
 HS1Pinkfoot = unique(simbag[Species == 'Pinkfoot' & SeasonNumber == 1, .(PolygonRef, NoShot)])
 file = 'o:/ST_LandskabsGenerering/temp/ReclassTestData/'
-write.table(HS1Pinkfoot, file = file.path(file, 'HS1Pinkfoot.txt'),  col.names = FALSE, row.names = FALSE, quote = FALSE, sep = ' : ')
+setnames(HS1Pinkfoot, c('PolyRefNum', 'Numbers'))
+write.csv(HS1Pinkfoot, file = file.path(file, 'HS1Pinkfoot2.csv'), row.names = FALSE, quote = FALSE)
