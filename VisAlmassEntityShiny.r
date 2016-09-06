1c1b1cfebd730aa2ec435dee10f1606c4cd5ec13
LDalby <lars@bios.au.dk>
2016-09-06 10:27:12 +0200

    Fix problem with holes in polygons

--- VisAlmassEntityShiny.r
+++ VisAlmassEntityShiny.r
@@ -20,38 +20,21 @@ DTvejlerne[, roworder:=orignialorder,]  # needed to restore order after merge.
 # setkey(DTvejlerne, 'PolyRefNum')
 joined = merge(DTvejlerne, entity, all.x = TRUE, by = 'PolyRefNum')
 vejlerne@data = joined[match(orignialorder, roworder),]
-
-vejlmap = SpatialPolygons2map(vejlerne, namefield = 'PolyRefNum')
-
-polyrefs = map(vejlmap, plot = FALSE)$names
-cols = rep('white', length(polyrefs))
-cols[!is.na(joined[,Numbers])] = 'blue'
-map(vejlmap, fill= TRUE, col = cols)
-x11()
 spplot(vejlerne, 'Numbers', col = 'lightgrey')
 
+polyswithbirds = joined[!is.na(Numbers), roworder]
+vejlmap = SpatialPolygons2map(vejlerne, namefield = 'roworder')
+
+mapnames = map(vejlmap, namesonly = TRUE)  # the roworder and now contains "duplicates" as in xx:1 and xx:2
+# this is from polygons with holes (e.g. a lake)
+getfirst = function(x) {stringr::str_split(x, ":")[[1]][1]}
+stripmapnames = sapply(mapnames, FUN = getfirst)
+cols = rep('white', length(mapnames))
+cols[match(polyswithbirds, mapnames)] = 'blue' 
+cols[which(mapnames %in% polyswithbirds)] = 'red' 
+cols[match(polyswithbirds, stripmapnames)] = 'green' 
+x11()
+map(vejlmap, fill= TRUE, col = cols)
 
 
 
-#---- 
- data(unemp)
- data(county.fips)
-
-  # define color buckets
-  colors = c("#F1EEF6", "#D4B9DA", "#C994C7", "#DF65B0", "#DD1C77", "#980043")
-  unemp$colorBuckets <- as.numeric(cut(unemp$unemp, c(0, 2, 4, 6, 8, 10, 100)))
-  leg.txt <- c("<2%", "2-4%", "4-6%", "6-8%", "8-10%", ">10%")
-
-  # align data with map definitions by (partial) matching state,county
-  # names, which include multiple polygons for some counties
-  cnty.fips <- county.fips$fips[match(map("county", plot=FALSE)$names,
-    county.fips$polyname)]
-  colorsmatched <- unemp$colorBuckets [match(cnty.fips, unemp$fips)]
-
-  # draw map
-  map("county", col = colors[colorsmatched], fill = TRUE, resolution = 0,
-    lty = 0, projection = "polyconic")
-  map("state", col = "white", fill = FALSE, add = TRUE, lty = 1, lwd = 0.2,
-    projection="polyconic")
-  title("unemployment by county, 2009")
-  legend("topright", leg.txt, horiz = TRUE, fill = colors)
