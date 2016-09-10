4e81fa11d97efce632cb3682bccd78fc6befe464
LDalby <lars@bios.au.dk>
2016-09-10 09:35:43 +0200

    Collect scenario output

--- ScenarioBackup.R
+++ ScenarioBackup.R
@@ -29,4 +29,63 @@ for (i in seq_along(scenariodirs)) {
     file.copy(file.path(thewd, 'HuntingBagRecord.txt'), file.path(OutDirectory, paste(tmp, Sys.Date(), 'HuntingBagRecord.txt')))
   }
 }
-  
+
+
+# ----
+# Get the untouced result files 
+pth = 'e:/almass/WorkDirectories/Goose/'
+dirs = dir(pth)
+scenariodirs = dirs[grep('WD2', dirs)]  # For the full model scenarios
+scenariodirs = c(scenariodirs, c('WD31', 'WD32', 'WD32', 'WD33', 'WD34', 'WD35', 'WD36', 'WD37', 'WD38'))
+# scenariodirs = c('WD20', 'WD21', 'WD22')
+foragelist = vector('list', length(scenariodirs))
+huntingoplist = foragelist
+huntingbaglist = foragelist
+
+
+for (i in seq_along(scenariodirs)) {
+  foragepth = file.path(pth, scenariodirs[i], 'GooseFieldForageData.txt')
+  sel = c('SeasonNumber',
+          'Day',
+          'Polyref',
+          'Barnacle',
+          'Pinkfoot',
+          'Greylag')
+  forage = fread(foragepth, select = sel)
+  forage[, Month := month(as.IDate(Day, origin = '2010-01-01'))]
+  forage[, Day := NULL]
+  forage = melt(
+    forage,
+    id.vars = c('SeasonNumber', 'Month', 'Polyref'),
+    value.name = 'Numbers',
+    variable.name = 'Species'
+  )
+  forage[, AvgNum := round(mean(Numbers)), by = c('SeasonNumber', 'Month', 'Polyref', 'Species')]
+  forage[, Numbers := NULL]
+  forage = forage[AvgNum > 0, ]
+  forage = unique(forage)
+  thescenario = readLines(file.path(pth, scenariodirs[i], 'ParameterValues.txt'))
+  forage[, Scenario := thescenario]
+  foragelist[[i]] = forage
+
+# Hunting bag
+simbagpth = file.path(pth, scenariodirs[i], 'HuntingBagRecord.txt')
+simbag = fread(simbagpth, drop = c('Hour', 'Minute', 'x-coord', 'y-coord', 'Year'))
+simbag[, Species:=sapply(GameType, ConvertGameType)]
+simbag[, NoShot:=.N, by = list(PolygonRef, Species, SeasonNumber)]
+simbag[, Scenario:=thescenario]
+huntingbaglist[[i]] = simbag
+}
+theforagelist = rbindlist(foragelist)
+write.table(theforagelist, 'o:/ST_GooseProject/ALMaSS/Scenarios/Scenario09092016/TheForageFiles.txt', row.names = FALSE, quote = FALSE)
+huntingbaglist = rbindlist(huntingbaglist)
+write.table(huntingbaglist, 'o:/ST_GooseProject/ALMaSS/Scenarios/Scenario09092016/TheHuntingbagFiles.txt', row.names = FALSE, quote = FALSE)
+
+
+
+
+
+
+
+
+
