7531be68a17f1d1c3f130589c5f590936e081e82
Lars Dalby <lars.dalby@gmail.com>
2017-01-06 15:34:09 +0100

    Add the slurm id to the paths

--- Grendel/StartParamFit.sh
+++ Grendel/StartParamFit.sh
@@ -1,14 +1,69 @@
 #!/bin/bash
-
+cd /home/ldalby/workspace/Goose/ParamFitting
 chmod +x FileDist.r
-./FileDist.r
+/home/com/R/3.1.2/bin/Rscript FileDist.r $SLURM_JOBID
 
-cd /home/lars/ALMaSS/tempdirectory/WD1
+cd /scratch/$SLURM_JOBID/GooseParamFit/WD1
 chmod +x _01_BatchLoop.sh
 ./_01_BatchLoop.sh > out &
 sleep 5
 
-cd /home/lars/ALMaSS/tempdirectory/WD2
+cd /scratch/$SLURM_JOBID/GooseParamFit/WD2
+chmod +x _01_BatchLoop.sh
+./_01_BatchLoop.sh > out &
+sleep 5
+
+cd /scratch/$SLURM_JOBID/GooseParamFit/WD3
+chmod +x _01_BatchLoop.sh
+./_01_BatchLoop.sh > out &
+sleep 5
+
+cd /scratch/$SLURM_JOBID/GooseParamFit/WD4
+chmod +x _01_BatchLoop.sh
+./_01_BatchLoop.sh > out &
+sleep 5
+
+cd /scratch/$SLURM_JOBID/GooseParamFit/WD5
+chmod +x _01_BatchLoop.sh
+./_01_BatchLoop.sh > out &
+sleep 5
+
+cd /scratch/$SLURM_JOBID/GooseParamFit/WD6
+chmod +x _01_BatchLoop.sh
+./_01_BatchLoop.sh > out &
+sleep 5
+
+cd /scratch/$SLURM_JOBID/GooseParamFit/WD7
+chmod +x _01_BatchLoop.sh
+./_01_BatchLoop.sh > out &
+sleep 5
+
+cd /scratch/$SLURM_JOBID/GooseParamFit/WD8
+chmod +x _01_BatchLoop.sh
+./_01_BatchLoop.sh > out &
+sleep 5
+
+cd /scratch/$SLURM_JOBID/GooseParamFit/WD9
+chmod +x _01_BatchLoop.sh
+./_01_BatchLoop.sh > out &
+sleep 5
+
+cd /scratch/$SLURM_JOBID/GooseParamFit/WD10
+chmod +x _01_BatchLoop.sh
+./_01_BatchLoop.sh > out &
+sleep 5
+
+cd /scratch/$SLURM_JOBID/GooseParamFit/WD11
+chmod +x _01_BatchLoop.sh
+./_01_BatchLoop.sh > out &
+sleep 5
+
+cd /scratch/$SLURM_JOBID/GooseParamFit/WD12
+chmod +x _01_BatchLoop.sh
+./_01_BatchLoop.sh > out &
+sleep 5
+
+cd /scratch/$SLURM_JOBID/GooseParamFit/WD13
 chmod +x _01_BatchLoop.sh
 ./_01_BatchLoop.sh > out &
 sleep 5
