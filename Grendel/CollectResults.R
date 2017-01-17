da1381dfb4af47124beef8ced700063bbf69fd93
Lars Dalby <lars.dalby@gmail.com>
2017-01-17 11:48:31 +0100

    Add script to collect all the result files

---
+++ Grendel/CollectResults.R
@@ -0,0 +1,29 @@
+# Collect results from parameter fitting
+# Author: Lars Dalby
+# Date: 17 Jan 2017
+# Short script to combine the results form mulitple runs into one file and 
+# write that to a safe place on Grendel.
+
+# Set the local R package library (if not done already)
+if(!'/home/ldalby/R/packages' %in% .libPaths()) {
+	.libPaths('/home/ldalby/R/packages')
+}
+
+library(data.table)
+# Get the slurm job-id (is being passed in from the shell script calling this R script)
+args = commandArgs(trailingOnly=TRUE)
+
+# The parent directory of all the work directories
+pathtodirs = file.path('/scratch', args)
+pathtofile = file.path(pathtodirs, paste0(basename,i))
+filename = 'ParameterFittingResults.txt'
+npar = 13  # Specifies the number of run directories that was being used
+basename = 'WD'  # The prefix to the directories
+# Read all the files into a list
+reslist = vector('list', npar)
+for (i in 1:npar) 
+{
+	reslist[[i]] = fread(file.path(pathtofile, filename))
+}
+allres = rbindlist(reslist)
+fwrite(allres, file = '/home/ldalby/workspace/Goose/ParamFitting/Results')
