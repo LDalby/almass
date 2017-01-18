2fb2c443eb80426cb3a7d11e649cc4290010dfb8
Lars Dalby <lars.dalby@gmail.com>
2017-01-18 21:10:13 +0100

    Use for loop

--- Grendel/StartParamFit.sh
+++ Grendel/StartParamFit.sh
@@ -3,80 +3,12 @@ cd /home/ldalby/workspace/Goose/ParamFitting
 chmod +x FileDist.r
 /home/com/R/3.1.2/bin/Rscript FileDist.r $SLURM_JOB_ID
 
-cd /scratch/$SLURM_JOB_ID/WD1
-chmod +x _01_BatchLoop.sh
-./_01_BatchLoop.sh > out &
-sleep 5
-
-cd /scratch/$SLURM_JOB_ID/WD2
-chmod +x _01_BatchLoop.sh
-./_01_BatchLoop.sh > out &
-sleep 5
-
-cd /scratch/$SLURM_JOB_ID/WD3
-chmod +x _01_BatchLoop.sh
-./_01_BatchLoop.sh > out &
-sleep 5
-
-cd /scratch/$SLURM_JOB_ID/WD4
-chmod +x _01_BatchLoop.sh
-./_01_BatchLoop.sh > out &
-sleep 5
-
-cd /scratch/$SLURM_JOB_ID/WD5
-chmod +x _01_BatchLoop.sh
-./_01_BatchLoop.sh > out &
-sleep 5
-
-cd /scratch/$SLURM_JOB_ID/WD6
-chmod +x _01_BatchLoop.sh
-./_01_BatchLoop.sh > out &
-sleep 5
-
-cd /scratch/$SLURM_JOB_ID/WD7
-chmod +x _01_BatchLoop.sh
-./_01_BatchLoop.sh > out &
-sleep 5
-
-cd /scratch/$SLURM_JOB_ID/WD8
-chmod +x _01_BatchLoop.sh
-./_01_BatchLoop.sh > out &
-sleep 5
-
-cd /scratch/$SLURM_JOB_ID/WD9
-chmod +x _01_BatchLoop.sh
-./_01_BatchLoop.sh > out &
-sleep 5
-
-cd /scratch/$SLURM_JOB_ID/WD10
-chmod +x _01_BatchLoop.sh
-./_01_BatchLoop.sh > out &
-sleep 5
-
-cd /scratch/$SLURM_JOB_ID/WD11
-chmod +x _01_BatchLoop.sh
-./_01_BatchLoop.sh > out &
-sleep 5
-
-cd /scratch/$SLURM_JOB_ID/WD12
-chmod +x _01_BatchLoop.sh
-./_01_BatchLoop.sh > out &
-sleep 5
-
-cd /scratch/$SLURM_JOB_ID/WD13
-chmod +x _01_BatchLoop.sh
-./_01_BatchLoop.sh > out &
-sleep 5
-
-cd /scratch/$SLURM_JOB_ID/WD14
-chmod +x _01_BatchLoop.sh
-./_01_BatchLoop.sh > out &
-sleep 5
-
-cd /scratch/$SLURM_JOB_ID/WD15
-chmod +x _01_BatchLoop.sh
-./_01_BatchLoop.sh > out &
-sleep 5
+for i in {1..15}; do
+	cd /scratch/$SLURM_JOB_ID/WD"$i"
+	chmod +x _01_BatchLoop.sh
+	./_01_BatchLoop.sh > out &
+	sleep 5
+done
 
 wait
 
