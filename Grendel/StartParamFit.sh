c04cffd9c3011d19e45f7ae036f4dea30fba5d08
Lars Dalby <lars.dalby@gmail.com>
2017-01-31 13:22:28 +0100

    Rem debugging code

--- Grendel/StartParamFit.sh
+++ Grendel/StartParamFit.sh
@@ -9,11 +9,9 @@ for i in {1..15}; do
 	./_01_BatchLoop.sh > out &
 	sleep 5
 done
-echo "For loop done before wait"
+
 wait
-echo "...after wait"
+
 cd /home/ldalby/workspace/Goose/ParamFitting
-echo "navigated to home dir"
 chmod +x CollectResults.r
 /home/com/R/3.1.2/bin/Rscript CollectResults.r $SLURM_JOB_ID
-echo "have called R script"
