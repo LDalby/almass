a99420f02acf20a3da0cd2c45898f4672badc976
Lars Dalby <lars.dalby@gmail.com>
2017-02-16 21:06:29 +0100

    Finish off first version of standard run files

--- Grendel/FileDistStandardRun.r
+++ Grendel/FileDistStandardRun.r
@@ -18,7 +18,7 @@ basedir = '/home/ldalby/workspace/Goose/RunDirectory'
 # The parent directory of all the work directories
 pathtodirs = file.path('/scratch', args)
 # Setup the directory
-npar = 1  # Specifies the number of run directories
+npar = 2  # Specifies the number of run directories
 basename = 'WD'  # The prefix to the directories
 # Make the directories and copy the files:
 for (i in 1:npar) 
@@ -30,17 +30,11 @@ for (i in 1:npar)
 }
 
 dirs = dir(pathtodirs) 
-# Edit the scripts to reflect the correct paths:
-for (i in seq_along(dirs)) {
-	wd = file.path(pathtodirs, dirs[i])
-	AppendWorkDir(WorkDir = wd, InScript = file.path(wd,'goosebatchr.r'), OutScript = 'batchr.r') 
-}
 
-# Edit the bat, ini and cfg files to match the parameters set above:
+# Edit the ini and cfg files to reflect the correct number of years and weather file
 years = 10  # the number of seasons to run (goose sims run over the year boundary)
 for (i in seq_along(dirs)) {
 	wd = file.path(pathtodirs, dirs[i])
-	EditBat(wd)
 	EditIni(WorkDir = wd, Model = 'goose', NYear = years+1)
 	EditConfig(file = file.path(wd, 'TIALMaSSConfig.cfg'), config = 'GOOSE_MODELEXITDAY', value = 365+134+years*365)
 	EditConfig(file = file.path(wd, 'TIALMaSSConfig.cfg'), config = 'MAP_WEATHER_FILE', value = 'Vejlerne2013-2014.pre')
--- Grendel/StartStandardRun.sh
+++ Grendel/StartStandardRun.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-cd /home/ldalby/workspace/Goose/ParamFitting
-chmod +x FileDist.r
-/home/com/R/3.1.2/bin/Rscript FileDist.r $SLURM_JOB_ID
+cd /home/ldalby/workspace/Goose/StandardRun
+chmod +x FileDistStandardRun.r
+/home/com/R/3.1.2/bin/Rscript FileDistStandardRun.r $SLURM_JOB_ID
 
-for i in {1..15}; do
+for i in {1..2}; do
 	cd /scratch/$SLURM_JOB_ID/WD"$i"
-	chmod +x _01_BatchLoop.sh
-	./_01_BatchLoop.sh > out &
+	# Run ALMaSS
+	./almass_cmdline > out
 	sleep 5
 done
 
 wait
 
-cd /home/ldalby/workspace/Goose/ParamFitting
-chmod +x CollectResults.r
-/home/com/R/3.1.2/bin/Rscript CollectResults.r $SLURM_JOB_ID
+cd /home/ldalby/workspace/Goose/StandardRun
+chmod +x CollectResultsStandardRun.r
+/home/com/R/3.1.2/bin/Rscript CollectResultsStandardRun.r $SLURM_JOB_ID
