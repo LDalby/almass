e34a874f29f5b7cad164ae4c37e55214c3ff1832
ldalby <lars.dalby@gmail.com>
2017-07-31 10:33:55 +0200

    Add dection of OS and adapt paths

--- render.r
+++ render.r
@@ -22,13 +22,22 @@ document()
 install_github('LDalby/loggr')
 
 
+if ("Linux" == sysinfo[match("sysname", names(sysinfo))]) {
+  gitpath = '~/git/almass/'
+  outpath <- "~/Desktop/"
+}
+if ("Windows" == sysinfo[match("sysname", names(sysinfo))]) {
+  gitpath = 'c:/MSV/WorkDirectory/'
+  outpath <- "o:/ST_Lada/Projekter/GBB/"
+}
+
 
 
 # Render rmd file
 library(rmarkdown)
 # Model testing
-render(input = "c:/Users/lada/Git/almass/GooseModelTesting.Rmd",
-       output_dir = "o:/ST_Lada/Projekter/GBB/",
+render(input = file.path(gitpath, "GooseModelTesting.Rmd"),
+       output_dir = outpath,
        output_file = paste0("GooseModelTesting", Sys.Date(), ".html"))
 
 # Parameter fitting
