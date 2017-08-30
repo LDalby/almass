7bc92ca681d3ee9927e218cbd14f659dcf2eff4e
ldalby <lars.dalby@gmail.com>
2017-08-30 10:28:21 +0200

    WIP temporal flock sizes

--- temporal_flocksizes.r
+++ temporal_flocksizes.r
@@ -19,19 +19,69 @@ vejlerne_polys %>%
 obs <- read_excel(file.path(o_drive, "/Field data/Fugledata/PolyAndObsJune2016/Observationer_fugledata_20160616.xlsx"),
                   sheet = "Observationer") %>% 
   mutate(PolyID = as.numeric(PolygonID),
-         ObsDato = ymd_hms(ObsDato)) %>% 
-  filter(hour(ObsDato) > 11 & hour(ObsDato) < 13)
+         ObsDato = ymd_hms(ObsDato)) # %>% 
+  # filter(hour(ObsDato) > 11 & hour(ObsDato) < 13)
 
 obs %>% 
   semi_join(y = vejlerne_polys, by = "PolyID") %>% 
-  mutate(Date = if_else(month(ObsDato) < 7, month(ObsDato) + 12, month(ObsDato))) %>% 
-  select(Date, ArtID, Antal) %>% 
+  mutate(Date = ifelse(month(ObsDato) < 7, month(ObsDato) + 12, month(ObsDato)),
+         Species = recode(ArtID,
+                          `63` = "Pinkfoot",
+                          `67` = "Barnacle",
+                          `59` = "Greylag")) %>% 
+  dplyr::select(Date, Species, Antal) %>% 
   filter(!is.na(Antal),
-         ArtID %in% c(63, 59, 67)) %>% 
-  group_by(Date, ArtID) %>% 
-  summarise(mean_flocksize = mean(Antal)) %>% 
-  ggplot(aes(Date, mean_flocksize)) +
-  geom_point(aes(color = ArtID))
+         Species %in% c("Pinkfoot", "Greylag", "Barnacle")) %>% 
+  group_by(Date, Species) %>% 
+  summarise(mean_flocksize = mean(Antal),
+            max_flocksize = max(Antal),
+            min_flocksize = min(Antal)) %>%  
+  ggplot(aes(Date, max_flocksize, color = Species)) +
+  geom_line() +
+  geom_point() +
+  scale_color_colorblind() +
+  theme_fivethirtyeight() + 
+  ggtitle("Max flock size per month", subtitle = "Field data up until June 2016")
+
+
+# Simulated
+resultpath <- '~/ALMaSS/WorkDirectory/'
+coltypes = "iiiiiiiiiiiidddddcddicc"
+forage <- read_tsv(file.path(resultpath, 'GooseFieldForageData.txt'), col_types = coltypes) %>% 
+  mutate(Day = Day - 365, Date = as.Date(Day, origin = as.Date('2009-01-01'))) %>% 
+  filter(month(Date) %in% c(8:12,1:3), Geese > 0) %>% 
+  ClassifyHabitatUse(species = 'goose', timed = TRUE)
+
+dists1 <- forage %>% 
+  dplyr::select(Day, BarnacleTimed, PinkfootTimed, GreylagTimed) %>% 
+  mutate(ObsDato = as.Date(Day, origin = '2014-01-01'),
+         month = month(ObsDato)) %>% 
+  filter(month %in% c(9:12,1:3)) %>% 
+  dplyr::select(-Day) %>% 
+  gather_(key_col = 'Species', value_col = 'Numbers', gather_cols = c("BarnacleTimed", "PinkfootTimed", "GreylagTimed")) %>% 
+  filter(Numbers != 0) %>% 
+  mutate(Species = recode(Species, 
+                          "BarnacleTimed" = "Barnacle",
+                          "PinkfootTimed" = "Pinkfoot",
+                          "GreylagTimed" = "Greylag"),
+         month = ifelse(month < 7, month + 12, month)) %>% 
+  group_by(Species, month) %>% 
+  summarise(mean_flocksize = mean(Numbers),
+            max_flocksize = max(Numbers),
+            min_flocksize = min(Numbers))
+  dists1 %>% 
+  ggplot(aes(month, mean_flocksize, color = Species)) +
+  geom_line() +
+  geom_point() +
+  scale_color_colorblind() +
+  theme_fivethirtyeight() + 
+  ggtitle("Max flock size per month", subtitle = "Simulated data")
+
+         
+
+
+
+
 
 
 
