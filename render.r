603fd55b4bb2677ca3274eab585a3d9449799dda
ldalby <lars.dalby@gmail.com>
2017-09-27 09:51:05 +0200

    Render to folder on desktop

--- render.r
+++ render.r
@@ -1,8 +1,11 @@
 # Short script to render rmd files 
+
+# dir.create("~/Desktop/Goose/")
+
 sysinfo <- Sys.info()
 if ("Linux" == sysinfo[match("sysname", names(sysinfo))]) {
   gitpath = '~/git/almass/'
-  outpath <- "~/Desktop/"
+  outpath <- "~/Desktop/Goose/"
 }
 if ("Windows" == sysinfo[match("sysname", names(sysinfo))]) {
   gitpath = 'c:/MSV/WorkDirectory/'
@@ -14,7 +17,7 @@ library(rmarkdown)
 # Model testing
 render(input = file.path(gitpath, "GooseModelTesting.Rmd"),
        output_dir = outpath,
-       output_file = paste0("GooseModelTesting", Sys.Date(), ".html"))
+       output_file = paste0("GooseModelTesting", Sys.Date(), "_C.html"))
 
 # Parameter fitting
 render(input = "c:/Users/lada/Git/almass/ParameterFittingGoose.Rmd",
